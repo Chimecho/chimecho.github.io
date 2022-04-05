dcb426fcfa9f22ebdaeb3b07e2e214cbb64afc2a
Carlos Espinoza <Chimecho@users.noreply.github.com>
2022-04-04 19:38:47 -0500

    Update main.js

--- switch_games_list/js/main.js
+++ switch_games_list/js/main.js
@@ -40,7 +40,7 @@ nxGamesCollection.controller('MainController', ['$scope', '$http', '$sce', funct
   $scope.games = []
   $scope.filteredGames = []
 
-  $http.get('data/games.json?v=0.8')
+  $http.get('data/games.json?v=0.81')
   .then(response => {
     $scope.games = _.map(response.data, game => {
       return {
